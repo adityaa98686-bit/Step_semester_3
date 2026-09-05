d8b59a6b808820fbed7d68303c257e61556606bf
aditya98686 <ak2975@srmist.edu.in>
2026-09-05 10:50:46 +0530

    project strcture is implemented

---
+++ src/main/java/topic_name/assignment_problems/SampleJavaFIle.java
@@ -0,0 +1,4 @@
+package topic_name.assignment_problems;
+
+public class SampleJavaFIle {
+}
---
+++ src/main/java/topic_name/class_problems/SampleJavaFile.java
@@ -0,0 +1,8 @@
+package topic_name.class_problems;
+
+public class SampleJavaFile {
+
+    public static void main(String[] args) {
+        System.out.println(" hello ");
+    }
+}
